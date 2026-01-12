3ac6e5d081c3a71d5782e5473ad236103bea0a01
sqwizee <77798787+SharicW@users.noreply.github.com>
2026-01-13 02:42:42 +0400

    Update run_parsers_loop.py

--- app/run_parsers_loop.py
+++ app/run_parsers_loop.py
@@ -1,19 +1,22 @@
+import os
 import time
 from app.stats import parse_sanctum, parse_solscan
 
-INTERVAL = int(time.getenv("PARSER_INTERVAL_SEC", "300"))
+INTERVAL = int(os.getenv("PARSER_INTERVAL_SEC", "300"))
 
 if name == "main":
     while True:
         try:
-            print("Sanctum:", parse_sanctum())
+            print("Sanctum...")
+            print(parse_sanctum())
         except Exception as e:
             print("Sanctum error:", e)
 
         try:
-            print("Solscan:", parse_solscan(limit_rows=25))
+            print("Solscan...")
+            print(parse_solscan(limit_rows=25))
         except Exception as e:
             print("Solscan error:", e)
 
-        print("Sleeping", INTERVAL, "sec")
+        print(f"Sleep {INTERVAL}s...")
         time.sleep(INTERVAL)
